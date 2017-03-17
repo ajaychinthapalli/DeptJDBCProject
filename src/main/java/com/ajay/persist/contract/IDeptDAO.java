b46dec67a2b3103976de95a513971906387834e1
AJAY\Ajay Chinthapalli <ajaychinthapallijava@gmail.com>
2017-03-17 00:50:01 -0400

    testing

--- src/main/java/com/ajay/persist/contract/IDeptDAO.java
+++ src/main/java/com/ajay/persist/contract/IDeptDAO.java
@@ -4,7 +4,5 @@ import com.ajay.persist.domain.DepartmentDO;
 import com.ajay.persist.exceptions.PersistException;
 
 public interface IDeptDAO {
-
 	public DepartmentDO getDeptByPK(Integer deptId) throws PersistException;
-
 }
